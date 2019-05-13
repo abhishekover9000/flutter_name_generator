0cb61ebd207685fec3a70430c106dec6f5f3c471
WhiteCloud Apps <abhishek.chowdhury247@gmail.com>
2019-05-13 15:21:42 -0700

    update part 2

--- lib/main.dart
+++ lib/main.dart
@@ -8,14 +8,18 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Name Generator',
+      theme: ThemeData(        
+        primaryColor: Colors.white,
+      ),                        
       home: RandomWords(),
     );
   }
 }
 
 class RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _biggerFont = const TextStyle(fontSize: 18.0);
+  final Set<WordPair> _saved = Set<WordPair>();
+  final List<WordPair> _suggestions = <WordPair>[];
+  final TextStyle _biggerFont = const TextStyle(fontSize: 18.0);
 
   Widget _buildSuggestions() {
     return ListView.builder(
@@ -32,15 +36,61 @@ class RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair) {
+    final bool alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text( pair.asPascalCase, style: _biggerFont),
+      trailing: Icon( alreadySaved ? Icons.favorite : Icons.favorite_border, color: alreadySaved ? Colors.red : null ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
       );
   }
 
+  void _pushSaved(){
+  Navigator.of(context).push(
+    MaterialPageRoute<void>(
+      builder: (BuildContext context) {
+        final Iterable<ListTile> tiles = _saved.map(
+          (WordPair pair) {
+            return ListTile(
+              title: Text(
+                pair.asPascalCase,
+                style: _biggerFont,
+              ),
+            );
+          },
+        );
+        final List<Widget> divided = ListTile
+          .divideTiles(
+            context: context,
+            tiles: tiles,
+          )
+              .toList();
+
+        return Scaffold(         // Add 6 lines from here...
+          appBar: AppBar(
+            title: Text('Saved Suggestions'),
+          ),
+          body: ListView(children: divided),
+        );                       // ... to here.
+      },
+    ),
+  );
+  }
 
     @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("Startup Yadidimsayn Generator")), body: _buildSuggestions());
+    return Scaffold(appBar: AppBar(title: Text("Startup Yadidimsayn Generator"), actions: <Widget> [
+      IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+    ],
+    ),
+    body: _buildSuggestions());
   }
 
 }
